846d6c32e66f0c6ce31081058b629d5ce08a045c
Anvay Bhanap <anvay.bhanap@gmail.com>
2021-07-19 16:16:40 -0700

    added basic formatting of page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'axios';
 import axios from 'axios';
 
 class App extends React.Component {
@@ -26,9 +25,6 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        {/* <h1>City Explorer</h1>
-        <hr /> */}
-
         <input onChange = {(e) => this.setState({ searchQuery: e.target.value})}
         placeholder = "type city name here..." type="text" />
         <button onClick = {this.getLocation}>Explore!</button>
