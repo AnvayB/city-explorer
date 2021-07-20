f67978951cd173e948da39c6d06217cd0dba4fc3
Anvay Bhanap <anvay.bhanap@gmail.com>
2021-07-19 18:55:47 -0700

    error message, want to improve

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './App.css';
+import Alert from 'react-bootstrap/Alert';
 
 
 class App extends React.Component {
@@ -17,19 +18,24 @@ class App extends React.Component {
   
   getLocation = async(e) => {
     e.preventDefault(); //stops form from reloading and clearing inputs
-    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER}&q=${this.state.searchQuery}&format=json`;
+    try {
+      const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER}&q=${this.state.searchQuery}&format=json`;
     const response = await axios.get(API);
     console.log('LOCATION IQ DATA:', response);
-
     this.setState({ location: response.data[0]});
 
-    const MAP = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_EXPLORER}&center=${this.state.location.lat},${this.state.location.lon}&zoom=10`;
+
+    const MAP = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_EXPLORER}&center=${this.state.location.lat},${this.state.location.lon}&zoom=11`;
     const respond = await axios.get(MAP);
     console.log(this.state.map);
     this.setState({ map: respond.config.url});
-
-    
+    this.setState({ errorAlert: false})
+    } 
+    catch (error) {
+      this.setState({errors: error.response.status, errorAlert: true, map: '', location: {}})
+    }
   }
+
   
   render () {
     return (
@@ -37,6 +43,14 @@ class App extends React.Component {
         <h1>City Explorer</h1>
         <hr />
        
+        <Alert show={this.state.errorAlert} variant="warning">
+          Error {this.state.errors}: Unable to geocode
+          <Button varaint="warning" onClick={() => this.setState({errorAlert: false})}>
+            Close
+          </Button>
+        </Alert>
+
+
         <Form onSubmit = {this.getLocation}>
           <Form.Group>
             <Form.Label>City Name</Form.Label>
