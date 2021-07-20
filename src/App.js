692c2773e08a57b8b987bb1b57c07216abe3f296
Anvay Bhanap <anvay.bhanap@gmail.com>
2021-07-19 18:05:56 -0700

    finished card 3

--- src/App.js
+++ src/App.js
@@ -3,56 +3,62 @@ import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchQuery: '',
-      location: {}
+      location: {},
+      map: ''
     }
   }
   
   getLocation = async(e) => {
-    e.preventDefault();
+    e.preventDefault(); //stops form from reloading and clearing inputs
     const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER}&q=${this.state.searchQuery}&format=json`;
     const response = await axios.get(API);
     console.log('LOCATION IQ DATA:', response);
 
     this.setState({ location: response.data[0]});
+
+    const MAP = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_EXPLORER}&center=${this.state.location.lat},${this.state.location.lon}&zoom=10`;
+    const respond = await axios.get(MAP);
+    console.log(this.state.map);
+    this.setState({ map: respond.config.url});
+
+    
   }
-  
-  
-  
-  
   
   render () {
     return (
       <div>
         <h1>City Explorer</h1>
         <hr />
-        {/* <input onChange = {(e) => this.setState({ searchQuery: e.target.value})}
-        placeholder = "type city name here..." type="text" />
-        <button onClick = {this.getLocation}>Explore!</button> */}
        
         <Form onSubmit = {this.getLocation}>
-      <Form.Group>
-        <Form.Label>City Name</Form.Label>
-        <Form.Control id = "form" onChange = {(e) => this.setState({ searchQuery: e.target.value})} placeholder = "type city name here..." type="text" />
-        <Button variant="primary" type="submit">
-          Explore!
-        </Button>
-        <Form.Text>
-        <br />
-        Location: {this.state.location.display_name} <br />
-        Location ID: {this.state.location.place_id} <br />
-        Latitude: {this.state.location.lat} <br />
-        Longitude: {this.state.location.lon} <br />
-        </Form.Text>
-      </Form.Group>
-    </Form>
+          <Form.Group>
+            <Form.Label>City Name</Form.Label>
+            <Form.Control id = "form" onChange = {(e) => this.setState({ searchQuery: e.target.value})} placeholder = "type city name here..." type="text" />
+            <Button variant="primary" type="submit">
+              Explore!
+            </Button>
+            <Form.Text>
+            <br />
+            Location: {this.state.location.display_name} <br />
+            Location ID: {this.state.location.place_id} <br />
+            Latitude: {this.state.location.lat} <br />
+            Longitude: {this.state.location.lon} <br />
+            <img src = {this.state.map} alt=""/>
+            </Form.Text>
+          </Form.Group>
+        </Form>
+
+        
+
       </div>
     )
-  }
+  }s
 }
 
 export default App;
