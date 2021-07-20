bf83b44f4a162da4c709ae023c759f6bb7b31bf9
Anvay Bhanap <anvay.bhanap@gmail.com>
2021-07-19 17:17:48 -0700

    added bootstrapped form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,7 +12,8 @@ class App extends React.Component {
     }
   }
   
-  getLocation = async() => {
+  getLocation = async(e) => {
+    e.preventDefault();
     const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER}&q=${this.state.searchQuery}&format=json`;
     const response = await axios.get(API);
     console.log('LOCATION IQ DATA:', response);
@@ -26,22 +29,27 @@ class App extends React.Component {
     return (
       <div>
         <h1>City Explorer</h1>
-        <input onChange = {(e) => this.setState({ searchQuery: e.target.value})}
+        <hr />
+        {/* <input onChange = {(e) => this.setState({ searchQuery: e.target.value})}
         placeholder = "type city name here..." type="text" />
-        <button onClick = {this.getLocation}>Explore!</button>
-
-        <p>
-          Location: {this.state.location.display_name}
-        </p>
-        <p>
-          Location ID: {this.state.location.place_id}
-        </p>
-        <p>
-          Latitude: {this.state.location.lat}
-        </p>
-        <p>
-          Longitude: {this.state.location.lon}
-        </p>
+        <button onClick = {this.getLocation}>Explore!</button> */}
+       
+        <Form onSubmit = {this.getLocation}>
+      <Form.Group>
+        <Form.Label>City Name</Form.Label>
+        <Form.Control id = "form" onChange = {(e) => this.setState({ searchQuery: e.target.value})} placeholder = "type city name here..." type="text" />
+        <Button variant="primary" type="submit">
+          Explore!
+        </Button>
+        <Form.Text>
+        <br />
+        Location: {this.state.location.display_name} <br />
+        Location ID: {this.state.location.place_id} <br />
+        Latitude: {this.state.location.lat} <br />
+        Longitude: {this.state.location.lon} <br />
+        </Form.Text>
+      </Form.Group>
+    </Form>
       </div>
     )
   }
